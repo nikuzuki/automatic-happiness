2d99e82de6ae3638f473983556ac76ec304a17d8
nikuzuki <nikuzuki29@gmail.com>
2018-12-12 04:34:20 +0900

    ログインできたら、flashメッセージを出す

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  get '/users/auth/:provider/callback' => 'sessions#create'
+  get '/auth/:provider/callback' => 'sessions#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
